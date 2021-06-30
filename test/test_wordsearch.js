1ed6f8088c1edc19e36e4ad5c70513244470ed7c
Sylas Serné <its.sylas@gmail.com>
2021-06-30 23:47:43 +0000

    pair programming with Yves

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,23 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY')
+
+    assert.isTrue(result);
+  });
+  
+  it("should return false if the word is not present vertically", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +50,21 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+  
+  it("should return false if the word is not present horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRIENDS')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+// turn array element into string
+//search string for word with .includes(word)
+//transpose matrix
+//do the same thing.
+
+const transpose = (matrix) => {
+  const transposed = [];
+  for (let col = 0; col < matrix[0].length; col++) {
+    const column = matrix.map((row)=>row[col])
+    transposed.push(column)
+  }
+  return transposed
 }
 
+const wordSearchHorizontal = (letters, word) => { 
+  for (const line of letters) {
+    const horizontalJoin = line.join('');
+    if (horizontalJoin.includes(word)) return true;
+  }
+  return false
+}
+
+const wordSearch = (letters, word) => {
+  if (wordSearchHorizontal(letters, word)) return true
+  if (wordSearchHorizontal(transpose(letters), word)) return true
+  return false
+}
+  
 module.exports = wordSearch
+
+  // console.log(transpose(
+  // [[1, 2, 3],
+  //  [4, 5, 6]]
+  // ))
+
+//[1,2,3]
+//[4,5,6]
+
+  //[1,4]
+  //[2,5]
+  //[3,6]
